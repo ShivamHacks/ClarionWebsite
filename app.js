71908253b45664b21c1cb27d70b740f373d57380
Shivam Agrawal <shivamlego@gmail.com>
2016-06-27 20:30:17 -0400

    cleaned up the code

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-//var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -8,11 +7,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Start Server
+
 app.listen(process.env.PORT || '3000', function () {
   console.log('Server started on port: ' + this.address().port);
 });
 
-// Article add
+// Libraries
 
 var multipart = require('connect-multiparty');
 var multipartMiddleware = multipart();
@@ -30,55 +31,14 @@ var s3 = new AWS.S3();
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 var Cookies = require('cookies');
+var jwt = require('jsonwebtoken');
+var secret = "lololol";
 
-function generateSampleData() {
-  var authors = [ 'Shivam Agrawal', 'Dhruv Agrawal' ];
-  var categories = [ 'news', 'opinions', 'comedy' ];
-  var dateRange = [ 42536, 42546 ];
-  var Items = [];
-  for (var i = 0; i < 100; i++) {
-    var rand1 = Math.floor(Math.random() * (authors.length));
-    var rand2 = Math.floor(Math.random() * (categories.length));
-    var rand3 = Math.floor((Math.random() * (dateRange[1] - dateRange[0])) + dateRange[0]);
-    //console.log(rand1 + "," + rand2 + "," + rand3);
-    var author = authors[rand1];
-    var category = categories[rand2];
-    var datePublished = rand3 + "";
-    var title = "Title " + i;
-    Items.push({
-      "urlCategory": { "S": category.replace(/\s+/g, '-').toLowerCase() },
-      "urlTitle": { "S": title.replace(/\s+/g, '-').toLowerCase() },
-      "urlAuthor": { "S": author.replace(/\s+/g, '-').toLowerCase() },
-      "category": { "S": category },
-      "title": { "S": title },
-      "datePublished": { "N": datePublished },
-      "author": { "S": author }
-    });
-  }
-  loadSampleData(Items);
-  //console.log(Items);
-}
+var LineByLineReader = require('line-by-line');
+var Parser = require("simple-text-parser");
+var parser = new Parser();
 
-function loadSampleData(Items) {
-  Items.forEach(function(Item) {
-    var params = {
-      TableName: "articleTable",
-      Item: Item
-    };
-    db.putItem(params, function(err, data) {
-     if (err) { console.log(err); }
-     else { console.log("Item putted"); }
-   });
-  });
-}
-//generateSampleData();
-
-
-// Router functions -------------------------------------------
-
-// table for 5 most recent from each category?
-// or have another table with key = urlAuthor and sort = urlTitle, then 3rd attribute = category
-// then I can simple query against this to get title and category, then i can get the rest, or simply maintain two tables
+// Public router functions
 
 app.get('/', function (req, res) {
   res.render('index', { title: "LOL" });
@@ -113,7 +73,6 @@ app.get('/article/:category/:title', function (req, res) {
   });
 });
 
-// TODO, limit to 10 order of closest date
 app.get('/category/:category', function (req, res) {
   var category = req.params.category;
   var params = categoryGetParams(category);
@@ -128,22 +87,7 @@ app.get('/category/:category', function (req, res) {
   });
 });
 
-// check back on this!
-app.get('/author/:author', function (req, res) {
-  var author = req.params.author;
-  var params = categoryGetParams(author);
-  docClient.query(params, function(err, data) {
-    if (err) { res.json(err); } 
-    else { res.json(data); }
-  });
-});
-
-// Editor Functions
-
-// there will be a preset list of approved editors in the form of a json file.
-
-var jwt = require('jsonwebtoken');
-var secret = "lololol";
+// Editor Router Functions
 
 app.use('/editor', function(req, res, next) {
   if (req.originalUrl == '/editor/login') { next(); }
@@ -154,20 +98,6 @@ app.use('/editor', function(req, res, next) {
     else res.send('NOT AUTHORIZED');
   }
 });
-
-function authorized(token) {
-  try {
-    var user = jwt.verify(token, secret);
-    var approved = require('./approved.json').approved;
-    var found = _.find(approved, function (u) { 
-      return u.name == user.name && u.pass == user.pass; 
-    });
-    if (found) return true;
-    else return false;
-  } catch(err) {
-    return false;
-  }
-}
 
 app.get('/editor/login', function (req,res,next) {
   var cookies = new Cookies(req, res);
@@ -215,13 +145,11 @@ app.post('/editor/newArticle/:id', multipartMiddleware, function (req, res) {
     var html = parseContent(id, article.content);
     s3TxtUpload(id, article.content, html);
     delete article.content;
-    // in callback, add type (error or not error)
     articlePut(id, article, function(data) {
       res.redirect('/article/' + linkify(article.category) + '/' + linkify(article.title));
     });
   } else {
     res.send("ERROR");
-    // redirect to an error page or send error back to client
   }
 });
 
@@ -237,8 +165,6 @@ app.post('/editor/upload/:id', upload.single('file'), function( req, res, next )
 });
 
 app.get('/editor/editArticle/:category/:title', function (req, res) {
-  // unused images are NOT deleted
-  // text resource is overwritten
   var category = req.params.category;
   var title = req.params.title;
   var params = articleGetParams(category, title);
@@ -279,6 +205,7 @@ app.get('/editor/directory', function (req, res) {
   });
 });
 
+// DB params
 
 var articleGetParams = function(category, title) {
   return {
@@ -315,24 +242,7 @@ var authorGetParams = function(author) {
   };
 };
 
-// Router functions -------------------------------------------
-
-/* AWS format
-
-  DynamoDB - this is wrong!
-  - 1 table titled 'articlesTable'
-  - 3 global secondary indexes:
-    - urlTitleIndex
-    - urlCategoryIndex
-    - urlAuthorIndex
-
-  S3
-  - 1 bucket titled 'clarionimgs'
-  - 1 bucket titled 'clarionarticles'
-
-  */
-
-// S3 functions -------------------------------------------
+// S3 Functions
 
 function s3ImgUpload(name, path, id, callback) {
   var imagePath = path;
@@ -371,10 +281,9 @@ function s3TxtUpload(id, txtContent, htmlContent) {
     else {  }
   });
 }
-// S3 functions -------------------------------------------
 
+// Helper Functions
 
-// Helper functions -------------------------------------------
 function deleteFile(yes, path) {
   if (yes) {
     fs.unlink(path, function(err) {
@@ -394,7 +303,6 @@ function noNullVals(obj) {
   return empty;
 }
 
-
 function extractData(obj) {
   var article = {};
   for (var key in obj) {
@@ -403,22 +311,25 @@ function extractData(obj) {
   return article;
 }
 
-// plus more cleaning?
-function cleanString(str) {
-  return str.trim();
-}
-
 function linkify(str) {
   return str.trim().replace(/\s+/g, '-').toLowerCase();
 }
-// Helper functions -------------------------------------------
 
+function authorized(token) {
+  try {
+    var user = jwt.verify(token, secret);
+    var approved = require('./approved.json').approved;
+    var found = _.find(approved, function (u) { 
+      return u.name == user.name && u.pass == user.pass; 
+    });
+    if (found) return true;
+    else return false;
+  } catch(err) {
+    return false;
+  }
+}
 
-// Parsing functions -------------------------------------------
-
-var LineByLineReader = require('line-by-line');
-var Parser = require("simple-text-parser");
-var parser = new Parser();
+// Parser
 
 parser.addRule(/\[(.*?)\]/, function(tag) {
   var src = "https://s3.amazonaws.com/clarionimgs/" + tag.replace(/[[\]]/g,'');
@@ -434,164 +345,7 @@ function parseContent(id, content) {
   return html;
 }
 
-/* Parser
-
-  - image: [imagename.type]
-  - hedaer: { header }
-
-  */
-
-
-
-
-// possibly add new rule for paragraphs???
-// add <q q> for quote - no, don't
-
-
-/*function parseDoc(fileLoc, id, callback) {
-  var lr = new LineByLineReader(fileLoc);
-  var html = "<span class='txt'>";
-  lr.on('line', function (line) {
-    if (line == "") { 
-      html += "<br>";
-    else {
-      var withId = 
-    html += parser.render(withId);
-  });
-  lr.on('end', function () {
-    html += "</span>"
-    callback(html);
-  });
-}*/
-
-// Parsing functions -------------------------------------------
-
-/* Parser:
-
-  image: [image.type] !!! no spaces
-  // accepts all image types. make sure if img is jpeg, type jpg in editor.
-  bold: <b text in bold b>
-  italics: <i text in italics i>
-  underline: <u text in underline u>
-  newline: \n
-
-  Not included (yet):
-  - no inline images
-  - no tabs in articles
-  - no captions for images
-
-  TODO
-  - add article categories (news, etc)
-  - delete imgs from upload folder
-  - make better content management system. i.e edit articles and delete cloud objects
-
-  - navbar & other styles
-  - search
-  - make look cool
-  */
-
-// add recent articles query later
-
-/*
-var bRegex = "<b(.*)b>";
-var iRegex = "<i(.*)i>";
-var uRegex = "<u(.*)u>";
-
-// var testRE = test.match("cow(.*)milk");
-
-function boRegex(line) {
-  var match = line.match(bRegex);
-  if (match != null)
-    return "<b>" + match[1].trim() + "</b>";
-}
-*/
-
-// img width - max 100%, img height- max 50vh, make it a block, so it is a new line. and end paragraph there.
-// make html wrap b/c its all going to be text.
-// classes - img: .img, text: .txt
-
-// here, i get article by title. This is the main way to go
-  // maybe do some fancy stuff with the date and category, etc to make url look cool
-  // make sure to plan this out first!!!
-  // and when done, try to make it open source, but first show it to clarion peeps.
-  // worst comes to worst, i'll use it for my own blog
-  // open source this blog software b/c i essentially wrote my own language!!!
-
-
-/*app.get('/article/:title', function (req, res) {
-  var title = req.params.title;
-  queryArticle(title, function(data) {
-    if (_.isEmpty(data)) {
-      res.send("ERROR, article not found, 404");
-    } else {
-      var id = data.Items[0].id.S;
-      getArticle(id, function(data) {
-        if (_.isEmpty(data)) {
-          res.send("ERROR, invalid article id");
-        } else {
-          var article = extractData(data.Item);
-          // get similar articles from category
-
-
-          var params = { Bucket: txtStorage, Key: id };
-          s3.getObject(params, function(err, data) {
-            if (err) { 
-              console.log(err, err.stack);
-              res.send("ERROR, could not find text resource");
-            }
-            else {
-              var buffer = new Buffer(data.Body);
-              var body = buffer.toString('utf-8');
-
-              res.render('article', {
-                article: article,
-                body: body,
-                urlBase: "localhost:3000",
-                header: {
-                  img: "http://shivamhacks.github.io/imgs/opener-background.jpeg",
-                  color: "#000000"
-                }
-              });
-            }
-          });
-        }
-      });
-    }
-  });
-});
-app.get('/category/:category', function (req, res) {
-  var category = req.params.category;
-  queryCategory(category, function(data) {
-    if (_.isEmpty(data)) {
-      res.send("ERROR, category not found, 404");
-    } else {
-      dbBatchGet(data.Items, function(data) {
-        res.render('articles_list', {
-          articles: data.Responses.articlesTable,
-          title: category,
-          urlBase: "localhost:3000"
-        });
-      });
-    }
-  });
-});
-app.get('/author/:author', function (req, res) {
-  var author = req.params.author;
-  queryAuthor(author, function(data) {
-    if (_.isEmpty(data)) {
-      res.send("ERROR, author not found, 404");
-    } else {
-      dbBatchGet(data.Items, function(data) {
-        res.render('articles_list', {
-          articles: data.Responses.articlesTable,
-          title: author,
-          urlBase: "localhost:3000"
-        });
-      });
-    }
-  });
-});*/
-
+// Final code
 
 app.use(function(err, req, res, next) {
   console.log(err);
@@ -599,5 +353,79 @@ app.use(function(err, req, res, next) {
   res.send("ERROR");
 });
 
-
 module.exports = app;
+
+
+
+// there will be a preset list of approved editors in the form of a json file.
+// table for 5 most recent from each category?
+// or have another table with key = urlAuthor and sort = urlTitle, then 3rd attribute = category
+// then I can simple query against this to get title and category, then i can get the rest, or simply maintain two tables
+  // unused images are NOT deleted
+  // text resource is overwritten
+      // redirect to an error page or send error back to client
+          // in callback, add type (error or not error)
+          // TODO, limit to 10 order of closest date
+
+          /* Parser
+
+  - image: [imagename.type]
+  - header: { header }
+
+*/
+
+
+/*
+function generateSampleData() {
+  var authors = [ 'Shivam Agrawal', 'Dhruv Agrawal' ];
+  var categories = [ 'news', 'opinions', 'comedy' ];
+  var dateRange = [ 42536, 42546 ];
+  var Items = [];
+  for (var i = 0; i < 100; i++) {
+    var rand1 = Math.floor(Math.random() * (authors.length));
+    var rand2 = Math.floor(Math.random() * (categories.length));
+    var rand3 = Math.floor((Math.random() * (dateRange[1] - dateRange[0])) + dateRange[0]);
+    //console.log(rand1 + "," + rand2 + "," + rand3);
+    var author = authors[rand1];
+    var category = categories[rand2];
+    var datePublished = rand3 + "";
+    var title = "Title " + i;
+    Items.push({
+      "urlCategory": { "S": category.replace(/\s+/g, '-').toLowerCase() },
+      "urlTitle": { "S": title.replace(/\s+/g, '-').toLowerCase() },
+      "urlAuthor": { "S": author.replace(/\s+/g, '-').toLowerCase() },
+      "category": { "S": category },
+      "title": { "S": title },
+      "datePublished": { "N": datePublished },
+      "author": { "S": author }
+    });
+  }
+  loadSampleData(Items);
+  //console.log(Items);
+}
+
+function loadSampleData(Items) {
+  Items.forEach(function(Item) {
+    var params = {
+      TableName: "articleTable",
+      Item: Item
+    };
+    db.putItem(params, function(err, data) {
+     if (err) { console.log(err); }
+     else { console.log("Item putted"); }
+   });
+  });
+}
+//generateSampleData();
+
+
+// check back on this!
+app.get('/author/:author', function (req, res) {
+  var author = req.params.author;
+  var params = categoryGetParams(author);
+  docClient.query(params, function(err, data) {
+    if (err) { res.json(err); } 
+    else { res.json(data); }
+  });
+});
+*/
